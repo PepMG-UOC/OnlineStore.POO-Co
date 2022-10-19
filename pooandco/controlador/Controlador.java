9d4b3bf0f64efc7d5e3a98b1f4933853d0eb8685
hernanUOC <115893667+hernanUOC@users.noreply.github.com>
2022-10-19 21:49:22 +0200

    Add files via upload

---
+++ pooandco/controlador/Controlador.java
@@ -0,0 +1,11 @@
+package pooandco.controlador;
+import pooandco.modelo.Datos;
+public class Controlador {
+
+    private Datos datos;
+    public Controlador() {
+        datos = new Datos ();
+    }
+// TO-BE-DONE
+
+}
